7dd18a8a42ecfaa168ee57d9477f6e841b10fce4
Narendra Yalavarthi <yalanar@amazon.com>
2023-02-17 15:22:18 -0500

    moved files into cdk folder

--- ebs-infra/src/cdk/bin/cdk.ts
+++ ebs-infra/src/cdk/bin/cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CognitoStack } from "../lib/cognito-stack";
+import { CognitoStack } from "./../../../../cognito-infra/src/cdk/lib/cognito-stack";
 import * as config from "../../../../environment-config";
 
 const app = new cdk.App();
