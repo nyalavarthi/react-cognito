4f6e4019741503819500788f45f3df5939d79f7b
Narendra Yalavarthi <yalanar@amazon.com>
2023-02-28 21:06:58 -0500

    add s3 bucket web hosting

--- s3-infra/src/cdk/bin/cdk.ts
+++ s3-infra/src/cdk/bin/cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkEbInfraStack } from "../lib/ebs-stack";
+import { CdkEbInfraStack } from "../lib/s3-stack";
 import * as config from "../../../../environment-config";
 
 const app = new cdk.App();
