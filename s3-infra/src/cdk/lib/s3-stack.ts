da3f706e8aebb83de4fe2d826e08a9490331cb46
Narendra Yalavarthi <yalanar@amazon.com>
2023-03-09 10:58:06 -0500

    corrected domain name

--- s3-infra/src/cdk/lib/s3-stack.ts
+++ s3-infra/src/cdk/lib/s3-stack.ts
@@ -4,9 +4,12 @@ import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as route53 from "aws-cdk-lib/aws-route53";
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as deploy from 'aws-cdk-lib/aws-s3-deployment';
+import { ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+
 
 export class CdkEbInfraStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props: cdk.StackProps) {
@@ -18,19 +21,25 @@ export class CdkEbInfraStack extends cdk.Stack {
             websiteErrorDocument: 'error.html',
             removalPolicy: cdk.RemovalPolicy.DESTROY
         })
-
+        /*
         const bucketPolicy = new iam.PolicyStatement({
             effect: iam.Effect.ALLOW,
             actions: ['s3:PutObject'],
             resources: [siteBucket.bucketArn + '/*'],
             principals: [new iam.AnyPrincipal()]
-        })
+        }) */
         //siteBucket.addToResourcePolicy(bucketPolicy)
         // Create a CloudFront Origin Access Identity (OAI)
         const oai = new cloudfront.OriginAccessIdentity(this, 'MyOAI');
-
+        
+        siteBucket.addToResourcePolicy(new iam.PolicyStatement({
+            actions: ['s3:GetObject'],
+            resources: [siteBucket.arnForObjects('*')],
+            principals: [new iam.CanonicalUserPrincipal(
+                oai.cloudFrontOriginAccessIdentityS3CanonicalUserId)],
+        }));
         // Grant the OAI access to the S3 bucket
-        siteBucket.grantRead(oai);
+        //siteBucket.grantRead(oai);
 
 
         // Create the Route 53 Hosted Zone
@@ -38,13 +47,14 @@ export class CdkEbInfraStack extends cdk.Stack {
             zoneName: config.HOST_NAME,
         });
 
-
         // Create a new SSL certificate in ACM
-        const cert = new acm.Certificate(this, "Certificate", {
+        const cert = new acm.DnsValidatedCertificate(this, "Certificate", {
             domainName: config.HOST_NAME,
-            validation: acm.CertificateValidation.fromDns(zone),
+            hostedZone: zone,
+            //validation: acm.CertificateValidation.fromDns(zone),
         });
 
+        /*
         const myViewerCertificate = cloudfront.ViewerCertificate.fromAcmCertificate(
             cert,
             {
@@ -53,6 +63,7 @@ export class CdkEbInfraStack extends cdk.Stack {
                 sslMethod: cloudfront.SSLMethod.SNI,
             },
         );
+        
 
         //Create CloudFront Distribution
         const siteDistribution = new cloudfront.CloudFrontWebDistribution(this, "SiteDistribution", {
@@ -68,23 +79,38 @@ export class CdkEbInfraStack extends cdk.Stack {
                     }]
                 }]
         });
+        */
 
-
-        //Create A Record Custom Domain to CloudFront CDN
-        new route53.ARecord(this, "SiteRecord", {
-            recordName: config.HOST_NAME,
-            target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(siteDistribution)),
-            zone
+        const cloudfrontDistribution = new cloudfront.Distribution(this, 'CloudFrontDistribution', {
+            certificate: cert,
+            domainNames: [config.HOST_NAME],
+            defaultRootObject: 'index.html',
+            defaultBehavior: {
+            origin: new origins.S3Origin(siteBucket, {
+                originAccessIdentity: oai
+            }),
+            viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            //responseHeadersPolicy: responseHeaderPolicy
+            },
         });
 
         //Deploy site to s3
         new deploy.BucketDeployment(this, "Deployment", {
             sources: [deploy.Source.asset("./../../../web/build")],
             destinationBucket: siteBucket,
-            distribution: siteDistribution,
+            distribution: cloudfrontDistribution,
             distributionPaths: ["/*"]
 
         });
+
+        //Create A Record Custom Domain to CloudFront CDN
+        new route53.ARecord(this, "SiteRecord", {
+            recordName: config.HOST_NAME,
+            target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(cloudfrontDistribution)),
+            zone
+        });
+
+        
 
 
         // Output the CloudFront distribution domain name
