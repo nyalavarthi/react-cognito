a03e244f19f8b91d7e23afa0e75b00229cb96eaa
Narendra Yalavarthi <nyalavarthi@gmail.com>
2023-05-26 12:30:54 -0400

    Update environment-config.ts

--- environment-config.ts
+++ environment-config.ts
@@ -2,14 +2,14 @@
 export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000/";
 
 //private sub for ebs
-export const PRIVATE_SUB1 = "subnet-42282b0f"
-export const PRIVATE_SUB2 = "subnet-f483fdd5"
+export const PRIVATE_SUB1 = ""
+export const PRIVATE_SUB2 = ""
 //Use if setting ELB to public.
-export const PUBLIC_SUB1 = "subnet-032f2355177bb1773"
-export const PUBLIC_SUB2 = "subnet-096dc2ede66cf4a21"
+export const PUBLIC_SUB1 = ""
+export const PUBLIC_SUB2 = ""
 
 //Cognito pool
-export const VPC_ID = "vpc-372ea24a"
+export const VPC_ID = ""
 export const COGNITO_USER_POOL_NAME = "sample-pool-showcase"
 export const COGNITO_DOMAIN_NAME = "sample-domain-showcase"
-export const env = { account: "718164670125", region: 'us-east-1' };
+export const env = { account: "", region: 'us-east-1' };
